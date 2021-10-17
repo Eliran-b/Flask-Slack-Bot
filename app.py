77e29aafb13461aeec5b7bd2384da26f8cc3f1b8
Eliran-b <eliran646@gmail.com>
2021-10-18 01:17:13 +0300

    fresh set up

--- app.py
+++ app.py
@@ -15,17 +15,15 @@ slack_token = str(subprocess.getstatusoutput(f'heroku config:get SLACK_TOKEN')[1
 client = slack.WebClient(token=slack_token)
 #time zone
 tz = pytz.timezone('Israel')
+
 #post message
-slack_signing_secret = str(subprocess.getstatusoutput(f'heroku config:get SIGNING_SECRET')[1])
+#slack_signing_secret = str(subprocess.getstatusoutput(f'heroku config:get SIGNING_SECRET')[1])
 #slack_event_adapter = SlackEventAdapter(slack_signing_secret, '/slack/events', app)
-client.chat_postMessage(channel='#content', text=str(datetime.now(tz).time()))
-
-
-
+#client.chat_postMessage(channel='#content', text=str(datetime.now(tz).time()))
 
 
 if __name__ == '__main__':
-    #app.run()
-    app.run(debug=True)  
+    app.run()
+    #app.run(debug=True)  
 
 
