ceb9503a1484cb2e74a5909f514b67f3825bec44
Eliran-b <eliran646@gmail.com>
2021-10-17 23:13:08 +0300

    fresh set up

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask
 from flask_restful import Api
-from db import db
-from security import bcrypt
-import subprocess
 import slack
+import os
+from pathlib import Path
+from dotenv import load_dotenv
 from datetime import datetime
 import pytz
 from slackeventsapi import SlackEventAdapter
@@ -11,31 +11,19 @@ from slackeventsapi import SlackEventAdapter
 
 app = Flask(__name__)
 
-#get the secret key and the uri from the heroku config var
-secret_key = str(subprocess.getstatusoutput(f'heroku config:get SECRET_KEY')[1])
-app.config['SECRET_KEY'] = secret_key
-db_uri = str(subprocess.getstatusoutput(f'heroku config:get DATABASE_URL')[1])
-app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 api = Api(app)
 
-db.init_app(app)
-bcrypt.init_app(app)
-
 #client config
-#get the token from the heroku config var
-token = str(subprocess.getstatusoutput(f'heroku config:get SLACK_TOKEN')[1])
-client = slack.WebClient(token=token)
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
+client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
 #time zone
 tz = pytz.timezone('Israel')
 #post message
-#get the secret key from heroku config vars
-'''
-signing_secret_key = str(subprocess.getstatusoutput(f'heroku config:get SLACK_SIGNING_SECRET')[1])
-slack_event_adapter = SlackEventAdapter(signing_secret_key, 'slack/events', app)
+slack_event_adapter = SlackEventAdapter(os.environ['SIGNING_SECRET'], 'slack/events', app)
 client.chat_postMessage(channel='#content', text=str(datetime.now(tz).time()))
-'''
 
 
 
--- commands.py
+++ commands.py
@@ -2,14 +2,6 @@ from app import app
 
 
 with app.app_context():
-    @app.cli.command("initdb")
-    def reset_db():
-        from db import db
-        db.drop_all()
-        db.create_all()
-
-
-
     @app.cli.command("script")
     def execute_script():
         print("Executing script...")
