0d1015b1546849e625f3a26b7037d949f4814121
Eliran-b <eliran646@gmail.com>
2021-10-18 19:44:36 +0300

    fresh set up

--- app.py
+++ app.py
@@ -5,21 +5,37 @@ from datetime import datetime
 import pytz
 from slackeventsapi import SlackEventAdapter
 import subprocess
+from apscheduler.schedulers.background import BackgroundScheduler
 
 app = Flask(__name__)
 
 api = Api(app)
 
+'''
+
+scheduler = BackgroundScheduler(daemon=True)
+
+#start thread
+scheduler.start()
+
+def send_msg(): 
+    tz = pytz.timezone('Israel')
+    msg = str(datetime.now(tz).hour)+":"+str(datetime.now(tz).minute)
+    client.chat_postMessage(channel='#content', text=msg)
+
+@app.before_first_request
+def init_schedule(): 
+    #create schedule email notification
+    scheduler.add_job(send_msg,'interval',minutes=60)
+
+'''
+
+
 #client config
 slack_token = str(subprocess.getstatusoutput(f'heroku config:get SLACK_TOKEN')[1])
 client = slack.WebClient(token=slack_token)
-#time zone
-tz = pytz.timezone('Israel')
-
-#post message
 slack_signing_secret = str(subprocess.getstatusoutput(f'heroku config:get SIGNING_SECRET')[1])
-#slack_event_adapter = SlackEventAdapter(slack_signing_secret, '/slack/events', app)
-#client.chat_postMessage(channel='#content', text=str(datetime.now(tz).time()))
+slack_event_adapter = SlackEventAdapter(slack_signing_secret, '/slack/events', app)
 
 
 if __name__ == '__main__':
@@ -27,3 +43,4 @@ if __name__ == '__main__':
     app.run(debug=True)  
 
 
+ 
