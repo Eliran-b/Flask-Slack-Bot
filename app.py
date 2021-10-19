0537c99c93f6ffb9fc0ea481774e99682e51bad7
Eliran-b <eliran646@gmail.com>
2021-10-19 09:06:39 +0300

    first route try

--- app.py
+++ app.py
@@ -34,7 +34,11 @@ def init_schedule():
 
 @app.route('/now', methods=['POST'])
 def now(): 
-    send_time_msg()
+    from datetime import datetime
+    import pytz
+    tz = pytz.timezone('Israel')
+    msg = str(datetime.now(tz).hour)+":"+str(datetime.now(tz).minute)
+    client.chat_postMessage(channel='#content', text=msg)
     
     
     '''
